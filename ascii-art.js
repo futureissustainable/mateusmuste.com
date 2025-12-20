2f183ab4122042a8168c2c4ec161e3c4519fe135
Random-tester45 <230830151+futureissustainable@users.noreply.github.com>
2025-12-21 00:05:55 +0200

    Update ascii-art.js

--- ascii-art.js
+++ ascii-art.js
@@ -4,19 +4,12 @@
 const ASCII_ART = {
     // Boot screen logo - ULTRA INT
     BOOT_LOGO: `                                                                                                                                                                          
-  :                                                               L.                      
-  Ef                i          j.                             t   EW:        ,ft          
-  E#t              LE GEEEEEEELEW,                   ..       Ej  E##;       t#E GEEEEEEEL
-  E#t             L#E ,;;L#K;;.E##j                 ;W,       E#, E###t      t#E ,;;L#K;;.
-  E#t            G#W.    t#E   E###D.              j##,       E#t E#fE#f     t#E    t#E   
-  E#t fi        D#K.     t#E   E#jG#W;            G###,       E#t E#t D#G    t#E    t#E   
-  E#t L#j      E#K.      t#E   E#t t##f         :E####,       E#t E#t  f#E.  t#E    t#E   
-  E#t L#L    .E#E.       t#E   E#t  :K#E:      ;W#DG##,       E#t E#t   t#K: t#E    t#E   
-  E#tf#E:   .K#E         t#E   E#KDDDD###i    j###DW##,       E#t E#t    ;#W,t#E    t#E   
-  E###f    .K#D          t#E   E#f,t#Wi,,,   G##i,,G##,       E#t E#t     :K#D#E    t#E   
-  E#K,    .W#G           t#E   E#t  ;#W:   :K#K:   L##,       E#t E#t      .E##E    t#E   
-  EL     :W##########Wt   fE   DWi   ,KK: ;##D.    L##,       E#t ..         G#E     fE   
-  :      :,,,,,,,,,,,,,.   :              ,,,      .,,        ,;.             fE      :                                                                                                    
+                                                     ___   ___                     
+    //   / / / /     /__  ___/ //   ) )  // | |         / /    /|    / / /__  ___/ 
+   //   / / / /        / /    //___/ /  //__| |        / /    //|   / /    / /     
+  //   / / / /        / /    / ___ (   / ___  |       / /    // |  / /    / /      
+ //   / / / /        / /    //   | |  //    | |      / /    //  | / /    / /       
+((___/ / / /____/ / / /    //    | | //     | |   __/ /___ //   |/ /    / /                                                                                                    
 `
 };
 
